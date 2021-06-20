47667396493c1f89f7397813b9b698999bab59ef
antrobot1234 <antrobot1234@gmail.com>
2021-06-20 10:25:47 -0500

    created config.lua

---
+++ config.lua
@@ -0,0 +1,29 @@
+local config = {
+    speed = 2.0,
+    fert = 1.5,
+    cave = 1.0,
+    rain = 1.0,
+    night = 1.5,
+    flowering = 0.5,
+    total = 0.001,
+    effect = {
+        none = 0.0,
+        aggressive = -1.0,
+        ends = -2.0,
+        poison = -1.0,
+        beatific = 1.0,
+        heroic = 1.5,
+        recharging = 1.5,
+        ravening = -1.0,
+        empowering = 2.5,
+        magnification = 2.0,
+        purifying = 2.5,
+        transmuting = 1.0
+    },
+    flowers = {
+        flowers = 1.0,
+        ["end"] = -2.0
+    }
+}
+
+return config
