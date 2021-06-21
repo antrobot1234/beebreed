9aea155fe64b56d8bc0d0258e9d63b5e3fb2abb1
antrobot1234 <antrobot1234@gmail.com>
2021-06-21 18:59:14 -0500

    didn't work

--- beebreed.lua
+++ beebreed.lua
@@ -71,7 +71,7 @@ local function getBeeSlot(side, rangemin, rangemax)
     if princessSlot and droneSlot then
         return math.min(princessSlot, droneSlot)
     else
-        return princessSlot or droneSlot or -1
+        return princessSlot or droneSlot
     end
 end
 local function moveItemToSlotRange(inSide, inSlot, outSide, outMin, outMax) --todo improve performance
